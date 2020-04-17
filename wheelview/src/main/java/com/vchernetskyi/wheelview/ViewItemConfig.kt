5face9c234237fc4f078343a4ebd321e7880f9e8
Vlad Chernetskyi <v.chernetskyi@eurospektras.lt>
2020-04-17 13:41:05 +0300

    Add vertical padding attribute

--- wheelview/src/main/java/com/vchernetskyi/wheelview/ViewItemConfig.kt
+++ wheelview/src/main/java/com/vchernetskyi/wheelview/ViewItemConfig.kt
@@ -2,5 +2,6 @@ package com.vchernetskyi.wheelview
 
 data class ViewItemConfig(
     val itemTextColor: Int,
-    val itemTextSize: Int
+    val itemTextSize: Int,
+    val itemVerticalPadding: Int = -1
 )
